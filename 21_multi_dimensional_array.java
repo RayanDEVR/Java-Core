bc500952f021e5b3447541a2d398102b620c9942
RayanDEVR <samiulrayan18@gmail.com>
2026-06-08 17:41:36 +0600

    22_jagged_array.java, 23_3d_array.java

--- 21_multi_dimensional_array.java
+++ 21_multi_dimensional_array.java
@@ -10,15 +10,6 @@ class MultiDimensionalArray
              nums[i][j] = (int) (Math.random() * 10);
          }
      }
-     
-        //  for (int i=0;i<3;i++)
-        //  {
-        //      for (int j = 0; j < 4; j++) {
-        //          System.out.print(nums[i][j] + " ");
-        //      }
-        //      System.out.println();
-        //  }
-         
 
 
          for (int n[] : nums)
