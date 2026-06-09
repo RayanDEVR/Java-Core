6e3496d68ff45ebe378f3788085b9bffe2cedcfc
RayanDEVR <samiulrayan18@gmail.com>
2026-06-10 01:43:59 +0600

    Enhanced the system

--- 13_switch_statement.java
+++ 13_switch_statement.java
@@ -6,53 +6,54 @@ class SwitchStatement
 {
    public static void main(String ar[])
    {
-      int n=66;
-             if(n==1)
+      
+       switch(n){
+       case 1:
        System.out.println("Saturday");
-       else if(n==2)
+       break;
+       case 2:
        System.out.println("Sunday");
-       else if(n==3)
+       break;
+       case 3:
         System.out.println("Monday");
-       else if(n==4)
+       break;
+       case 4:
         System.out.println("Tuesday");
-       else if(n==5)
+       break;
+       case 5:
        System.out.println("Wednesday");
-       else if(n==6)
+       break;
+       case 6:
        System.out.println("Thursday");
-       else if(n==7)
+       break;
+       case 7:
        System.out.println("Friday");
-       else
+       break;
+       default:
          System.out.println("Enter a valid number");
+       }
 
-      //  switch(n){
-      //  case 1:
+      // int n=66;
+      //        if(n==1)
       //  System.out.println("Saturday");
-      //  break;
-      //  case 2:
+      //  else if(n==2)
       //  System.out.println("Sunday");
-      //  break;
-      //  case 3:
+      //  else if(n==3)
       //   System.out.println("Monday");
-      //  break;
-      //  case 4:
+      //  else if(n==4)
       //   System.out.println("Tuesday");
-      //  break;
-      //  case 5:
+      //  else if(n==5)
       //  System.out.println("Wednesday");
-      //  break;
-      //  case 6:
+      //  else if(n==6)
       //  System.out.println("Thursday");
-      //  break;
-      //  case 7:
+      //  else if(n==7)
       //  System.out.println("Friday");
-      //  break;
-      //  default:
+      //  else
       //    System.out.println("Enter a valid number");
-      //  }
+
 
    }
 <<<<<<< HEAD
 }
 =======
-}
 >>>>>>> 79af2cbce711fd5671ebfbc64cec77e50d96b67d
