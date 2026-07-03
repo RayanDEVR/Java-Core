5c170902be21f3a8700b4a1f1db1b478aa7e76bb
RayanDEVR <samiulrayan18@gmail.com>
2026-07-03 12:02:15 +0600

    Use of inheritance

---
+++ 33_inheritance.java
@@ -0,0 +1,13 @@
+class Inheritance extends AdvCalc {
+    public static void main (String [] args)
+    {
+        AdvCalc obj = new AdvCalc();
+
+        int r1= obj.add(2,3);
+        int r2= obj.sub(4,1);
+        int r3= obj.multi(4,5);
+        double r4= obj.div(20,5);
+
+        System.out.println(r1 + " " +r2 + " "+ r3 + " " +r4);
+    }
+}
