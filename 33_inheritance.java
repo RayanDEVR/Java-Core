02a94d08a927524bd749fc340d785a9d1404a3c0
RayanDEVR <samiulrayan18@gmail.com>
2026-07-04 13:40:29 +0600

    Pacckages calculators file

--- 33_inheritance.java
+++ 33_inheritance.java
@@ -1,3 +1,5 @@
+import tools.*;
+
 class Inheritance extends AdvCalc {
     public static void main (String [] args)
     {
---
+++ tools/AdvCalc.java
@@ -0,0 +1,15 @@
+package tools;
+
+
+public class AdvCalc  extends Calc
+ {
+     public int multi(int a, int b) 
+    {
+        return a * b;
+    }
+    
+    public double div(double a, double b) 
+    {
+        return a / b;
+    }
+}
