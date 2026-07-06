3172bc92b000da3997f627fdab9dd67b76c1b818
RayanDEVR <samiulrayan18@gmail.com>
2026-07-06 18:30:31 +0600

    use of wrapper class

---
+++ 38_wrapper_class.java
@@ -0,0 +1,21 @@
+
+
+class WrapperClass
+{
+    public static void main (String [] args)
+    {
+        int num = 4;
+        Integer num1 = num;         //autoboxing
+
+        
+        int num2 = num1;            //auto-unboxing
+
+        System.out.println(num2);
+
+
+        String str = "12";
+        int num3 = Integer.parseInt(str);
+
+        System.out.println(num3 * 2);
+    }
+}
