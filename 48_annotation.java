10112dafbdcc25cc6dfda3b614b3d516d55dab10
RayanDEVR <samiulrayan18@gmail.com>
2026-07-13 17:46:20 +0600

    use of annotation

---
+++ 48_annotation.java
@@ -0,0 +1,27 @@
+class A 
+{
+    public void showTheDataWhichBelongsToThisClass() 
+    {
+        System.out.println("in A show");
+    }
+}
+
+class B extends A 
+{
+    @Override
+    public void showTheDataWhichBelongsToThisClass() 
+    {
+        System.out.println("in B show");
+    }
+}
+
+
+class Annotation 
+{
+    public static void main(String[] args) 
+    {
+        B obj = new B();
+        obj.showTheDataWhichBelongsToThisClass();
+   
+    }
+}
