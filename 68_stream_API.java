32293e2ab93abc0e442781b7b809a1cdbabd6892
RayanDEVR <samiulrayan18@gmail.com>
2026-07-23 17:29:53 +0600

    concept of Stream API

---
+++ 68_stream_API.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Demo {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(4, 5, 7, 2, 3, 8);
+
+        Stream<Integer> s1 = nums.stream();
+        // s1.forEach(n -> System.out.println(n));
+
+        Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
+        // s2.forEach(n -> System.out.println(n));
+
+        Stream<Integer> s3 = s2.map(n -> n * 2);
+        // s3.forEach(n -> System.out.println(n));
+
+        int result = s3.reduce(0, (c, e) -> c + e);
+        System.out.println(result);
+    }
+}
